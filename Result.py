3d64ad5501d85cb571a52e8266ecab7fd7bdbc77
rmohsen <mohr1360@gmail.com>
2018-07-04 18:57:37 +0430

    backbone
    back

---
+++ Result.py
@@ -0,0 +1,9 @@
+def calculate_first_question_answer(self):
+    pass
+
+
+import seaborn
+
+
+def calculate_second_question_answer(self):
+    pass
---
+++ Simulator.py
@@ -0,0 +1,35 @@
+class Processor:
+    def __init__(self, name, queue_capacity, timing):
+        self.name = name
+        self.queue_capacity = queue_capacity
+        self.timing = timing
+
+
+class Process:
+    pass
+
+
+class ProcessGenerator:
+    pass
+
+
+def srjf_resource_allocating(self):
+    pass
+
+
+def random_resource_allocating(self):
+    pass
+
+
+def ps_resource_allocating(self):
+    pass
+
+
+def fcfs_resource_allocating(self):
+    pass
+
+
+def precise_calculation(self):
+    pass
+
+
